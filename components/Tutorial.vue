8f174241bfa783468aa8476963d11d916067543a
Panachai <panachai.ny@gmail.com>
2021-11-09 17:27:41 +0700

    Update Tutorial.vue

---
+++ components/Tutorial.vue
@@ -0,0 +1,36 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <p>{{ response }}</p>
+    <button @click="share">Share นะจ๊ะ</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return { response: 'default response' }
+  },
+  methods: {
+    share() {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: 'title this is google Demo',
+            text: 'text this is google demo',
+            url: 'https://www.google.co.th',
+          })
+          .then(() => {
+            console.log('Thanks for sharing!')
+          })
+          .catch(console.error)
+      } else {
+        // fallback
+        console.log('fallback')
+      }
+    },
+  },
+})
+</script>
